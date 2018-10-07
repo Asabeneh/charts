999ae7be382222645bf1d1ac17400efb36f636c1
Asabeneh <asabeneh@gmail.com>
2018-10-07 13:32:55 +0300

    refactored

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ const data = [100, 150, 250, 300, 380, 400, 430, 460, 500];
 const chartWrapper = document.querySelector(".chart-wrapper");
 const addBarGraph = document.querySelector("#add-bar-graph");
 const addHistograph = document.querySelector("#add-histograph");
-const addPieChart = document.querySelector("#add-pie-chart");
-const addLineGraph = document.querySelector("#add-line-graph");
 const addCircleGraph = document.querySelector("#add-circle-graph");
 
-const createBarGraph = () => {
+const createBarGraph = data => {
   let div;
   data.forEach(number => {
     div = document.createElement("div");
@@ -15,12 +13,13 @@ const createBarGraph = () => {
     Object.assign(div.style, {
       width: (number / 500) * 100 + "%",
       backgroundColor: `rgba(0,10,30,${number / 500})`
-    })
+    });
     chartWrapper.appendChild(div);
   });
 };
+createBarGraph(data);
 
-const createHistograph = () => {
+const createHistograph = data => {
   let div;
   data.forEach(number => {
     div = document.createElement("div");
@@ -30,12 +29,12 @@ const createHistograph = () => {
       height: number + "px",
       top: 500 - number + "px",
       backgroundColor: `rgba(0,10,30,${number / 500})`
-    })
+    });
 
     chartWrapper.appendChild(div);
   });
 };
-const createCircleGraph = () => {
+const createCircleGraph = data => {
   let div;
   data.forEach(number => {
     div = document.createElement("div");
@@ -44,67 +43,57 @@ const createCircleGraph = () => {
       width: number / 4 + "px",
       height: number / 4 + "px",
       borderRadius: number / 2 + "px",
-      textAlign: 'center',
+      textAlign: "center",
       lineHeight: number / 4 + "px",
       display: "inline-block",
-      marginLeft: '3px',
+      marginLeft: "3px",
       fontSize: number / 20 + "px",
       backgroundColor: `rgba(0,10,30,${number / 500})`
-
-    })
+    });
     chartWrapper.appendChild(div);
   });
 };
 
 addBarGraph.addEventListener("click", () => {
   chartWrapper.innerHTML = "";
-  addHistograph.classList.remove('active');
-  addCircleGraph.classList.remove('active');
-  addBarGraph.className = 'active';
-  createBarGraph();
-
+  addHistograph.classList.remove("active");
+  addCircleGraph.classList.remove("active");
+  addBarGraph.className = "active";
+  createBarGraph(data);
 });
 
 addHistograph.addEventListener("click", () => {
   chartWrapper.innerHTML = "";
   if (window.innerWidth < 525) {
-    addHistograph.classList.remove('active');
-    addCircleGraph.classList.remove('active');
-    addBarGraph.className = 'active'
-    createBarGraph();
-
+    addHistograph.classList.remove("active");
+    addCircleGraph.classList.remove("active");
+    addBarGraph.className = "active";
+    createBarGraph(data);
+  } else {
+    addBarGraph.classList.remove("active");
+    addCircleGraph.classList.remove("active");
+    addHistograph.className = "active";
+    createHistograph(data);
   }
-  else {
-    addBarGraph.classList.remove('active');
-    addCircleGraph.classList.remove('active');
-    addHistograph.className = 'active'
-    createHistograph();
-  }
-
 });
-
 
 addCircleGraph.addEventListener("click", () => {
   chartWrapper.innerHTML = "";
-  addHistograph.classList.remove('active')
-  addBarGraph.classList.remove('active');
-  addCircleGraph.className = 'active'
-  createCircleGraph();
+  addHistograph.classList.remove("active");
+  addBarGraph.classList.remove("active");
+  addCircleGraph.className = "active";
+  createCircleGraph(data);
 });
 
-createBarGraph();
-
-window.addEventListener('resize', (e) => {
+window.addEventListener("resize", e => {
   chartWrapper.innerHTML = "";
   if (e.currentTarget.innerWidth <= 525) {
-    addHistograph.classList.remove('active')
-    addBarGraph.className = 'active'
-    createBarGraph();
-
-  }
-  else {
-    addBarGraph.classList.remove('active');
-    addHistograph.className = 'active'
-    createHistograph();
+    addHistograph.classList.remove("active");
+    addBarGraph.className = "active";
+    createBarGraph(data);
+  } else {
+    addBarGraph.classList.remove("active");
+    addHistograph.className = "active";
+    createHistograph(data);
   }
 });
